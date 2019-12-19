3360dbaea2576060700c4bbf258de2b1c8eea6ca
Nabil <benweb31@gmail.com>
2019-12-19 11:36:04 +0100

    first commit

---
+++ src/main/java/com/hacka/noel/NoelApplication.java
@@ -0,0 +1,13 @@
+package com.hacka.noel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hacka/noel/NoelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacka.noel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
