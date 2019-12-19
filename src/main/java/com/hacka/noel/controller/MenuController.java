4d99807dbf156614995efdbfdef447a06018f6a6
Nabil <benweb31@gmail.com>
2019-12-19 14:26:28 +0100

    Squellette

---
+++ src/main/java/com/hacka/noel/controller/MenuController.java
@@ -0,0 +1,40 @@
+package com.hacka.noel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MenuController {
+
+    @GetMapping("/")
+    public String home() {
+        return "indexWithModal";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/save-info")
+    public String saveInfo() {
+        // TODO : MYSQL
+        return "redirect:/index";
+    }
+
+    @GetMapping("/projet")
+    public String projet() {
+        return "projet";
+    }
+
+    @GetMapping("/glossaire")
+    public String glossaire() {
+        return "glossaire";
+    }
+
+    @GetMapping("/a-propos")
+    public String aPropos() {
+        return "a-propos";
+    }
+}
