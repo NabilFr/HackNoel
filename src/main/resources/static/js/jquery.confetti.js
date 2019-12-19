055a42d5d3fe1fbf3506e42ac98e33e161175c8a
Nabil <benweb31@gmail.com>
2019-12-19 19:01:32 +0100

    page projet fini, manque header et footer

---
+++ src/main/resources/static/js/jquery.confetti.js
@@ -0,0 +1,9 @@
+var mp = 150;
+
+var particleColors = {
+    colorOptions: ["DodgerBlue", "OliveDrab", "Gold", "pink", "SlateBlue", "lightblue", "Violet", "PaleGreen", "SteelBlue", "SandyBrown", "Chocolate", "Crimson"],
+    colorIndex: 0,
+    colorIncrementer: 0,
+    colorThreshold: 10
+};
+
