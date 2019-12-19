a0f03d9459503401aed2f77b0e44e665cd2c0f67
Nabil <benweb31@gmail.com>
2019-12-19 13:18:06 +0100

    ajout objet

--- src/main/java/com/hacka/noel/controller/QuestionController.java
+++ src/main/java/com/hacka/noel/controller/QuestionController.java
@@ -1,16 +1,17 @@
 package com.hacka.noel.controller;
 
+import com.hacka.noel.entity.Question;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.*;
 
 @Controller
 public class QuestionController {
 
+    Question question = new Question(0);
+
     @GetMapping("/")
     public String home() {
 
-        return "test1";
+        return "question1";
     }
 }
